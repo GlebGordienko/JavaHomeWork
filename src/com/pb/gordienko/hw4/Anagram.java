a682ba2808d4abdb9d05400c85b24cd96d8ca73b
Gleb Gordienko <gleb.gordienko@privatbank.ua>
2021-10-24 21:06:59 +0300

    init commit 6

---
+++ src/com/pb/gordienko/hw4/Anagram.java
@@ -0,0 +1,42 @@
+package com.pb.gordienko.hw4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    static String meth1(String A) {
+        return A.replaceAll("[^\\da-zA-Zа-яёА-ЯЁ ]", "");
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("введите строку");
+        String input = scan.next();
+        System.out.println("введите анаграмму");
+        String input2 = scan.next();
+
+        String inputchr = meth1(input);
+        char[] charArray = inputchr.toCharArray();
+        Arrays.sort(charArray);
+        String sortedString = new String(charArray);
+
+
+        String inputchr2 = meth1(input2);
+        char[] charArray2 = inputchr2.toCharArray();
+        Arrays.sort(charArray2);
+        String sortedString2 = new String(charArray2);
+
+
+        boolean result;
+        result = sortedString.equals(sortedString2);
+
+
+        if (result) {
+            System.out.println("Это анаграмма");
+        } else {
+            System.out.println("Это не анаграмма");
+        }
+
+
+    }
+}
