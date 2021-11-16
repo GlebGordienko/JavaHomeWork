8797f969f994a2a21541db56597a836dff6e925a
Gleb Gordienko <gleb.gordienko@privatbank.ua>
2021-11-16 11:29:12 +0200

    init commit

--- src/com/pb/gordienko/hw7/Atelier.java
+++ src/com/pb/gordienko/hw7/Atelier.java
@@ -4,14 +4,14 @@ package com.pb.gordienko.hw7;
 import com.pb.gordienko.hw6.Animal;
 
 public class Atelier {
-    static void dressWomen(Clothes...clothes) {
+    static void dressMan(Clothes...clothes) {
        // for(int i = 0; i < clothes.length; i++){
        // if(clothes[i] instanceof Skirt ){
         for(Clothes count: clothes){
-            if(count instanceof Skirt){
+            // if(count instanceof Skirt){
             System.out.println(count.getColor());
-        }
-        else{System.out.println();}}}
+        }}
+       // else{System.out.println();}}}
 
 
 
@@ -23,13 +23,14 @@ public class Atelier {
 
     public static void main(String[] args){
             Pants pants = new Pants(Size.XS, "black", 40);
-            Skirt skirt = new Skirt(Size.L, "red", 32);
+        Pants skirt = new Pants(Size.L, "red", 32);
             Tie tie = new Tie(Size.M, "yellow", 10);
             Tshirt tshirt = new Tshirt(Size.S, "green", 15);
             Clothes[] cloth = new Clothes[]{pants, skirt, tie, tshirt};
 
             pants.dressMan();
-            dressWomen(cloth);
+            dressMan(cloth);
 
-        }}
+        }
 
+}
