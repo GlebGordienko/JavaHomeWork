ca6fa56fdab39a3e6485b4a91278a7d7efd1cb8e
Gleb Gordienko <gleb.gordienko@privatbank.ua>
2021-11-20 23:47:14 +0200

    init commit

---
+++ src/com/pb/gordienko/hw8/OnlineShop.java
@@ -0,0 +1,10 @@
+package com.pb.gordienko.hw8;
+
+public class OnlineShop {
+    public static void main(String[] args) {
+        Auth auth = new Auth();
+        auth.signIn();
+        auth.signUp();
+
+    }
+}
